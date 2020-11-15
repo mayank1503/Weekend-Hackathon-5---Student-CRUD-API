447bda8c3446acccada40e7939966065aa4cd057
mayank1503 <mayankmotwani1503@gmail.com>
2020-11-15 21:29:08 +0530

    Final Test

--- src/index.js
+++ src/index.js
@@ -9,41 +9,39 @@ app.use(express.urlencoded());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.get('/api/student', (req, res) => {
-    res.send(studentArray);
+app.get('/api/student',(req,res) => {
+    res.send(data);
 });
 
-app.get('/api/student/:id', (req, res) => {
+app.get('/api/student/:id',(req,res) => {
     const id = req.params.id;
-    const student = studentArray.find(student => student.id === parseInt(id));
-
+    const student = data.find((student) => student.id === parseInt(id));
     if(!student) {
-        res.status(404).send(`Student with id ${id} was not found!`);
+        res.status(404);
         return;
     }
     res.send(student);
+
 });
 
-app.post('/api/student', (req, res) => {
+app.post('/api/student',(req,res) => {
     const {name, currentClass, division} = req.body;
     if((!name) || (!currentClass) || (!division)) {
         res.status(400);
         return;
     }
-
     const newStudent = {
-        id: studentArray.length + 1,
+        id: data.length + 1,
         name: name,
         currentClass: currentClass,
         division: division
-    };
-
-    studentArray.push(student);
-
+    }
+    data.push(newStudent);
     res.send(newStudent.id);
+
 });
 
-app.put('/api/v1/student/:id', (req, res) => {
+app.put('/api/student/:id',(req,res) => {
     const id = req.params.id;
     const student = data.find(student => student.id === parseInt(id));
     const newName = req.body.name;
@@ -54,7 +52,8 @@ app.put('/api/v1/student/:id', (req, res) => {
     student.name = newName;
 
 });
-app.delete('/api/v1/student/:id', (req, res) => {
+
+app.delete('/api/student/:id',(req,res) => {
     const id = req.params.id;
     const studentIndex = data.findIndex((student) => parseInt(id) === student.id);
     if(studentIndex === -1) {
