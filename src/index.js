066c3e3987e0bc547d0bd8a3a6eba9bea1c703a3
mayank1503 <mayankmotwani1503@gmail.com>
2020-11-15 21:30:59 +0530

    Final Test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
-const studentArray = require('./InitialData');
 const port = 8080
 app.use(express.urlencoded());
 
+const data = require('./initialData.js');
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+// your code goes here
+
 app.get('/api/student',(req,res) => {
     res.send(data);
 });
